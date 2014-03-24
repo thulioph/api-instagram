aa6cccc336aafe2a703e9aae064e81956ca8ee1b
Thulio Philipe <thulioph@gmail.com>
2014-03-24 19:06:45 -0300

    Atualizando e corrigindo bugs

--- js/instagram.js
+++ js/instagram.js
@@ -2,53 +2,50 @@
 // Puxando pela HASHTAG
 // ======================
 
-// var accessToken = '181941196.521a8ff.ecc245c88d2f42bca1455ae357e9e2cf';
-// var limit = 20; //Limite máximo de fotos
-// var setSize = "large";
-// var tag = "vidaarida"
+var accessToken = '181941196.5b9e1e6.c4df4ed6d9494b9a817be3ee7d046127';
+var limit = 20; //Limite máximo de fotos
+var setSize = "small";
+var tag = 'recife';
 
-// var instagram = function() {
-// 	return {
-// 		init: function() {
-// 			instagram.loadImages();
-// 		},
-// 		loadImages: function() {
-// 			var getImagesURL = 'https://api.instagram.com/v1/tags/'+ tag +'/media/recent?client_id=be52cb013dda4c47a03cdd5689896c37&access_token='+ accessToken +'';
-// 			$.ajax({
-// 				type: "GET",
-// 				dataType: "jsonp",
-// 				cache: false,
-// 				url: getImagesURL,
-// 				success: function(data) {
-// 					for(var i = 0; i < limit ; i+=1) {
-// 						if(setSize == "small") {
-// 							var size = data.data[i].images.thumbnail.url;
-// 						} else if(setSize == "medium") {
-// 							var size = data.data[i].images.low_resolution.url;
-// 						} else {
-// 							var size = data.data[i].images.standard_resolution.url;	
-// 						}
-// 						$("#lista-instagram").append("<figure><a target='_blank' href='" + data.data[i].link +"'><img src='" + size +"'/></a></figure>");
-// 					}
-// 				}
-// 			});
-// 		}
-// 	}
-// }();
+var instagram = function() {
+	return {
+		init: function() {
+			instagram.loadImages();
+		},
+		loadImages: function() {
+			var getImagesURL = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=' + accessToken + '';
+			// var getImagesURL = 'https://api.instagram.com/v1/tags/'+ tag +'/media/recent?client_id=9ec4a914b19846b694dca425582e089c&access_token='+ accessToken +'';
+			$.ajax({
+				type: "GET",
+				dataType: "jsonp",
+				cache: false,
+				url: getImagesURL,
+				success: function(data) {
+					for(var i = 0; i < limit ; i+=1) {
+						if(setSize == "small") {
+							var size = data.data[i].images.thumbnail.url;
+						} else if(setSize == "medium") {
+							var size = data.data[i].images.low_resolution.url;
+						} else {
+							var size = data.data[i].images.standard_resolution.url;	
+						}
+						$("#hashtag-list").append("<figure><a target='_blank' href='" + data.data[i].link +"'><img src='" + size +"'/></a></figure>");
+					}
+				}
+			});
+		}
+	}
+}();
 
-// $(document).ready(function() {
-//     instagram.init();
-// });
+instagram.init();
 
 
 // ======================
-// Puxando pelo usuário
+// Puxando pelo USUÁRIO
 // ======================
 
-// accessToken para validar a API, caso contrário não funciona;
-var accessToken = '181941196.521a8ff.ecc245c88d2f42bca1455ae357e9e2cf';
 // username de quem voc6e quer mostrar as imagens;
-var username= "thulioph";
+var IDuser= "18890326";
 // O limite máximo de fotos que podem ser mostradas são 20;
 var limit = 20;
 // Tamanho das imagens que iram aparecer, possíveis valores (small, medium, large);
@@ -58,41 +55,27 @@ var instagram = function() {
 	return {
 		// Inicia chamando as informações do usuário
 		init: function() {
-			instagram.getUser();
-		},
-		// Pega as informações do usuário
-		getUser: function() {
-			var getUserURL = 'https://api.instagram.com/v1/users/search?q='+ username +'&access_token='+ accessToken +'';
-			$.ajax({
-				type: "GET",
-				dataType: "jsonp",
-				cache: false,
-				url: getUserURL,
-				success: function(data) {
-					var getUserID = data.data[0].id;
-					instagram.loadImages(getUserID);
-				}
-			});
+			instagram.loadImages();
 		},
 		// Carrega as imagens
 		loadImages: function(userID) {
-			var getImagesURL = 'https://api.instagram.com/v1/users/'+ userID +'/media/recent/?access_token='+ accessToken +'';
+			var getImagesURL = 'https://api.instagram.com/v1/users/'+ IDuser +'/media/recent?client_id=9ec4a914b19846b694dca425582e089c&access_token='+ '';
 			$.ajax({
 				type: "GET",
 				dataType: "jsonp",
 				cache: false,
 				url: getImagesURL,
 				success: function(data) {
-					for(var i = 0; i < limit; i+=1) {
+					for(var i = 0; i < limit; i++) {
 						if(setSize == "small") {
 							var size = data.data[i].images.thumbnail.url;
 						} else if(setSize == "medium") {
 							var size = data.data[i].images.low_resolution.url;
 						} else {
-							var size = data.data[i].images.standard_resolution.url;
+							var size = data.data[i].images.standard_resolution.url;	
 						}
 						// Imprime no HTML a estrutura indicada, você pode alterar a forma como irá exibir no HTML;
-						$("#lista-instagram").append("<figure><a target='_blank' href='" + data.data[i].link +"'><img src='" + size +"'/></a></figure>"
+						$("#user-list").append("<figure><a target='_blank' href='" + data.data[i].link +"'><img src='" + size +"'/></a></figure>"
 							);
 					}
 				}
@@ -101,6 +84,4 @@ var instagram = function() {
 	}
 }();
 
-$(document).ready(function() {
-    instagram.init();
-});
+instagram.init();
